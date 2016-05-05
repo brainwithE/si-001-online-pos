038f6afeac9b1ecdde712599e03cc707dc51022f
mens-rea <juan.miguel.g.alvarez@gmail.com>
2016-05-05 14:51:49 +0800

    fix: oops missed something on report-inventory view

--- application/views/report-item.php
+++ application/views/report-item.php
@@ -6,6 +6,13 @@
 		<div class="overwatch-mec mec-income">
 			
 			<div class="col-xs-12">
+				<div class="row table-title table-title-general table-title-income">
+					<div class="col-xs-2">Item Code</div>
+					<div class="col-xs-2">Item Supplier</div>
+					<div class="col-xs-4">Item Name</div>
+					<div class="col-xs-2">Price</div>
+					<div class="col-xs-2">Stock on Hand</div>	
+				</div>
 				<?php
 				foreach($item->result_array() as $row){ 
 					$item_code = $row['item_id'];
@@ -14,13 +21,13 @@
 					$item_price = number_format($row['item_price'],2,'.',',');
 					$item_stock = $row['item_stock'];
 				?>
-					<div class="row table-entries table-entries-income">
-						<div class="col-xs-2"><?php echo $item_code;?></div>
-						<div class="col-xs-2"><?php echo $item_supplier;?></div>
-						<div class="col-xs-4"><?php echo $item_name;?></div>
-						<div class="col-xs-2"><?php echo $item_price;?></div>
-						<div class="col-xs-2"><?php echo $item_stock;?></div>	
-					</div>
+				<div class="row table-entries table-entries-income">
+					<div class="col-xs-2"><?php echo $item_code;?></div>
+					<div class="col-xs-2"><?php echo $item_supplier;?></div>
+					<div class="col-xs-4"><?php echo $item_name;?></div>
+					<div class="col-xs-2"><?php echo $item_price;?></div>
+					<div class="col-xs-2"><?php echo $item_stock;?></div>	
+				</div>
 
 				<?php } ?>
 			</div>

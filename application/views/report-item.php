27110e8fd4ce3d82a994c4bf8faa3bbbbf4c9892
mens-rea <juan.miguel.g.alvarez@gmail.com>
2016-05-05 14:49:59 +0800

    chore: cleaning report-item view

--- application/views/report-item.php
+++ application/views/report-item.php
@@ -1,125 +1,35 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?>
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
+	<div class="container">
 
-<!-- insert ponpon about content -->
+		<!--The overwatch Main element Container or MEC-->
+		<div class="overwatch-mec mec-income">
 			
+			<div class="col-xs-12">
+				<?php
+				foreach($item->result_array() as $row){ 
+					$item_code = $row['item_id'];
+					$item_supplier = $row['supplier_name'];
+					$item_name = $row['item_name'];
+					$item_price = number_format($row['item_price'],2,'.',',');
+					$item_stock = $row['item_stock'];
+				?>
+					<div class="row table-entries table-entries-income">
+						<div class="col-xs-2"><?php echo $item_code;?></div>
+						<div class="col-xs-2"><?php echo $item_supplier;?></div>
+						<div class="col-xs-4"><?php echo $item_name;?></div>
+						<div class="col-xs-2"><?php echo $item_price;?></div>
+						<div class="col-xs-2"><?php echo $item_stock;?></div>	
+					</div>
 
-				<div class="container">
+				<?php } ?>
+			</div>
 
-					<!--The overwatch Main element Container or MEC-->
-					<div class="overwatch-mec mec-income">
-						<div class="col-xs-12">
-							<!-- FILTER FUNCTION
+			<!-- <div class="table-title table-end table-end-general table-end-income">
+					<div class="col-xs-6 col-sm-9 total-label">TOTAL EARNINGS</div>
+					<div class="col-xs-3 total-amount"><?php echo $total_earnings; ?></div>
+			</div> -->
 
-							<div class="row">
-								<?php
-									//echo form_open('filter-income');							
-								?>
-								<input type="text" id="datepickerstart" class="datepicker" placeholder="Start Date" name="filter_start_date">
-								<input type="text" id="datepickerend" class="datepicker" placeholder="End Date" name="filter_end_date">
-								<?php
-									//echo form_submit(array('name'=>'submit','value'=>'FILTER','class'=>'call-links'));
-									//echo form_close();
-								?>
-							</div> -->
+		</div><!-- MEC end -->
 
-							<div class="row table-title table-title-general table-title-income">
-								<div class="col-xs-2">Item Code</div>
-								<div class="col-xs-2">Item Supplier</div>
-								<div class="col-xs-4">Item Name</div>
-								<div class="col-xs-2">Price</div>
-								<div class="col-xs-2">Stock on Hand</div>	
-							</div>
-							<?php
-								/*$total_earnings = 0;
-								foreach($income->result_array() as $row){ 
-								$income_name = $row['income_name'];
-								$income_amount = $row['income_amount'];
-								$income_date_acquired = $row['income_date_acquired'];
-								$total_earnings = $total_earnings + $income_amount;*/
-
-								foreach($item->result_array() as $row){ 
-									$item_code = $row['item_id'];
-									$item_supplier = $row['supplier_name'];
-									$item_name = $row['item_name'];
-									$item_price = number_format($row['item_price'],2,'.',',');
-									$item_stock = $row['item_stock'];
-
-							?>
-								
-								<div class="row table-entries table-entries-income">
-									<!-- <div class="col-xs-3"><?php $new_income_date_acquired //= //date("M j, Y", strtotime($income_date_acquired));  echo $new_income_date_acquired; ?></div>
-									<div class="col-xs-5 col-sm-6"><?php //echo $income_name; ?></div>
-									<div class="col-xs-3"><?php //echo $income_amount; ?></div> -->
-
-									<div class="col-xs-2"><?php echo $item_code;?></div>
-									<div class="col-xs-2"><?php echo $item_supplier;?></div>
-									<div class="col-xs-4"><?php echo $item_name;?></div>
-									<div class="col-xs-2"><?php echo $item_price;?></div>
-									<div class="col-xs-2"><?php echo $item_stock;?></div>	
-								</div>
-
-							<?php } ?>
-							
-						</div>
-
-						<?php if (!isset($ajax_req)): ?>
- 
-						<div class="show-gallery">
-						 
-						View only Gallery
-						</div>
-						 
-						 
-						<div class="show-images">
-						 
-						View only Images
-						</div>
-						 
-						 
-						<div class="show-articles">
-						 
-						View only Articles
-						</div>
-						 
-						<?php endif; ?>
-						  
-						 
-						<div id="ajax-content-container">
-						 
-						<table class="table table-bordered table-condensed table-striped">
-						 
-						<tr>
-						 
-						<th>Title</th>
-						 
-						 
-						<th>Type</th>
-						 
-						    </tr>
-						 
-						    <?php foreach ($node_list as $key=>$value): ?>
-						 
-						<tr>
-						 
-						<td><?php print $value->title; ?></td>
-						 
-						 
-						<td width="10%"><?php print ucfirst($value->type); ?></td>
-						 
-						      </tr>
-						 
-						    <?php endforeach; ?>
-						  </table>
-						 
-						</div>
-
-						<div class="table-title table-end table-end-general table-end-income">
-								<div class="col-xs-6 col-sm-9 total-label">TOTAL EARNINGS</div>
-								<div class="col-xs-3 total-amount"><?php echo $total_earnings; ?></div>
-						</div>
-					</div><!-- MEC end -->
-
-				</div>
+	</div>

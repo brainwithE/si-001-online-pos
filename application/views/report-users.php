d0ee2aa144b242708d819e478adae87e3120fa96
brainwithE <haylyzh@gmail.com>
2016-05-11 13:57:08 +0800

    fix: fix user details modal view

--- application/views/report-users.php
+++ application/views/report-users.php
@@ -2,6 +2,18 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
+<script>
+
+$(document).ready(function(){	
+    $('.btn-edit').click(function(){
+    	$('.user-details').hide();
+        /*$('.edit-user').show();
+        $('.user-details').hide();*/
+    });
+
+});
+
+</script>
 
 <!-- insert ponpon about content -->
 			
@@ -17,16 +29,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 						<div class="col-xs-12">						
 
 							<div class="row table-title table-title-general table-title-income">
+								<div class="col-xs-1"></div>
 								<div class="col-xs-2">User ID</div>
-								<div class="col-xs-1">Name</div>
-								<div class="col-xs-2">Email</div>
-								<div class="col-xs-2">Last Login</div>
-								<div class="col-xs-2">Last Activity</div>	
-								<div class="col-xs-2">Last Login Attempt</div>	
-								<div class="col-xs-1"></div>	
+								<div class="col-xs-3">Name</div>
+								<div class="col-xs-3">Email</div>									
+								<div class="col-xs-2"></div>	
 							</div>
 							<?php								
-
+								$ctr=0;
 								foreach($users as $row){ 
 									$user_id = $row->id;									
 									$user_name = $row->name;
@@ -36,38 +46,45 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 									$user_last_login = $row->last_login;
 									$user_last_activity = $row->last_activity;
 									$user_last_login_attempt = $row->last_login_attempt;
+									
 
 									if($is_banned == 0){
-
-
+										$ctr++;
 							?>
 								
-								<div class="row table-entries table-entries-income">
-
+								<div class="row table-entries table-entries-income" data-toggle="modal" <?php echo "data-target=#User".$user_id?>>
+									<div class="col-xs-1"><?php echo $ctr;?></div>
 									<div class="col-xs-2"><?php echo $user_id;?></div>
-									<div class="col-xs-1"><?php echo $user_name;?></div>
-									<div class="col-xs-2"><?php echo $user_email;?></div>
-									<div class="col-xs-2"><?php echo date("M j, Y", strtotime($user_last_login)); ?></div>
-									<div class="col-xs-2"><?php echo date("M j, Y", strtotime($user_last_activity)); ?></div>
-									<div class="col-xs-2"><?php echo date("M j, Y", strtotime($user_last_login_attempt)); ?></div>
-									<div class="col-xs-1">
+									<div class="col-xs-3"><?php echo $user_name;?></div>
+									<div class="col-xs-3"><?php echo $user_email;?></div>
+									<div class="col-xs-2">
 
-										<a href="#" class="btn" data-toggle="modal" <?php echo "data-target=#User".$user_id?>>Edit</a>
+										<a href="#" class="btn btn-edit" data-toggle="modal" <?php echo "data-target=#Edit".$user_id?>>Edit</a>
 										<a href="<?php echo base_url(); ?>authenticate/ban_user/<?php echo $user_id?>" class="btn-red"><i class="fa fa-times-circle" aria-hidden="true"></i></a>
 										
 									</div>
-									
-								</div>
 
-								<div class="modal fade" <?php echo "id='User".$user_id."'"?> role="dialog">
+
+									<div class="modal fade" <?php echo "id='Edit".$user_id."'"?> role="dialog" class="edit-user">
 					                <div class="modal-dialog">
 					                    <!-- Modal content-->
 					                    <div class="modal-content item-modal">					                    
 						                    <div class="edit-user">
 						                    	<div class="head-contain">
-													<h4><i class="fa fa-pencil-square-o" aria-hidden="true"></i>Edit User Details</h4>
+													<h4><i class="fa fa-exclamation-circle" aria-hidden="true"></i>User Information	</h4>
 												</div>
 						                    	<div class="modal-body modal-project">
+						                    	  <p><span>User ID:</span><?php echo $user_id;?></p>
+						                          <p><span>Name:</span><?php echo $user_name;?></p>
+						                          <p><span>Email:</span><?php echo $user_email;?></p>
+						                          <p><span>Last Login:</span><?php echo date("M j, Y g:i a", strtotime($user_last_login)); ?></p>
+						                          <p><span>Last Activity:</span><?php echo date("M j, Y g:i a", strtotime($user_last_activity)); ?></p>
+						                          <p><span>Last Login Attempt:</span><?php echo date("M j, Y g:i a", strtotime($user_last_login_attempt)); ?></p>
+						                        </div>
+						                        <div class="head-contain">
+													<h4><i class="fa fa-pencil-square-o" aria-hidden="true"></i>Edit User Details</h4>
+												</div>
+						                        <div class="modal-body modal-project">
 						                        	<?php echo form_open('admin/user-update'); ?>
 						                       	  		<input type="hidden" name="user_id" value="<?php echo $user_id; ?>">
 						                       	  		<label>User Name: </label>
@@ -86,7 +103,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					                    </div>
 					                    
 					                </div>
-					        </div>
+					       		</div>
+									
+								</div>
+
+								<!--edit-->
+								
+
 
 							<?php 
 								}
@@ -108,15 +131,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 						<div class="col-xs-12">						
 
 							<div class="row table-title table-title-general table-title-income">
+								<div class="col-xs-1"></div>
 								<div class="col-xs-2">User ID</div>
 								<div class="col-xs-1">Name</div>
 								<div class="col-xs-2">Email</div>
 								<div class="col-xs-2">Last Login</div>
-								<div class="col-xs-2">Last Activity</div>	
-								<div class="col-xs-2">Last Login Attempt</div>	
 								<div class="col-xs-1"></div>	
 							</div>
-							<?php								
+							<?php
+								$ctr=0;
 
 								foreach($users as $row){ 
 									$user_id = $row->id;									
@@ -125,24 +148,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 									$user_email = $row->email;									
 									$is_banned = $row->banned;			
 									$user_last_login = $row->last_login;
-									$user_last_activity = $row->last_activity;
-									$user_last_login_attempt = $row->last_login_attempt;
+									
 
 									if($is_banned == 1){
-
+										$ctr++;
 
 							?>
 								
 								<div class="row table-entries table-entries-income">
-
+									<div class="col-xs-1"><?php echo $ctr;?></div>
 									<div class="col-xs-2"><?php echo $user_id;?></div>
 									<div class="col-xs-1"><?php echo $user_name;?></div>
 									<div class="col-xs-2"><?php echo $user_email;?></div>
-									<div class="col-xs-2"><?php echo date("M j, Y", strtotime($user_last_login)); ?></div>
-									<div class="col-xs-2"><?php echo date("M j, Y", strtotime($user_last_activity)); ?></div>
-									<div class="col-xs-2"><?php echo date("M j, Y", strtotime($user_last_login_attempt)); ?></div>
+									<div class="col-xs-2"><?php echo date("M j, Y g:i a", strtotime($user_last_login)); ?></div>
 									<div class="col-xs-1">
-										<a href="<?php echo base_url(); ?>authenticate/ban_user/<?php echo $user_id?>" class="btn-green"><i class="fa fa-check-circle" aria-hidden="true"></i></a>
+										<a href="<?php echo base_url(); ?>authenticate/unban_user/<?php echo $user_id?>" class="btn-green"><i class="fa fa-check-circle" aria-hidden="true"></i></a>
 										
 									</div>
 									

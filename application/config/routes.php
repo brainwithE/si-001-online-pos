1998d014d4fd93c65c448800c2506ec65867292b
mens-rea <juan.miguel.g.alvarez@gmail.com>
2016-05-11 10:21:32 +0800

    fix: add sales link for cashier

--- application/config/routes.php
+++ application/config/routes.php
@@ -104,6 +104,7 @@ $route['add-delivery-transaction'] = 'tenant/add_delivery_transaction';
 //cashier actions
 
 $route['cashier/add-sales'] = 'cashier/add_sales';
+$route['add-sales'] = 'cashier/add_sales';
 $route['cashier/add-sales-transaction'] = 'cashier/add_sales_transaction';
 
 $route['cashier/report-sales'] = 'cashier/view_sales_report';
